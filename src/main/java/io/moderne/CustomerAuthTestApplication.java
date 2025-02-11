fbb29c0b073cac74edf0e18cc7cc03738dd99dbb
Peter Streef <p.streef@gmail.com>
2025-02-11 08:31:48 +0100

    initial commit

---
+++ src/main/java/io/moderne/CustomerAuthTestApplication.java
@@ -0,0 +1,13 @@
+package io.moderne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAuthTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerAuthTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/moderne/CustomerAuthTestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.moderne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerAuthTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
